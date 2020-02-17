01409f77f51a8b9545a4ad3e954e4a9d9e0f9e98
Filip106 <djordjevic.filip994@gmail.com>
2020-02-17 12:09:28 +0100

    removed docker dumps

--- dump/users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `sa_users` (
-    `id` INT NOT NULL AUTO_INCREMENT ,
-    `username` VARCHAR(100) NOT NULL ,
-    `email` VARCHAR(100) NOT NULL ,
-    PRIMARY KEY (`id`),
-    UNIQUE (`username`),
-    UNIQUE (`email`))
-ENGINE = InnoDB;
